c4e1cdbca84711124beef7c022764d4bcf7407da
vaibhavpatil-coditas <vaibhav.patil@coditas.com>
2026-06-01 12:12:40 +0530

    Created the tenant context and filter

---
+++ src/main/java/com/coditas/powerbridge/tenant/TenantContext.java
@@ -0,0 +1,21 @@
+package com.coditas.powerbridge.tenant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TenantContext {
+
+    private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>();
+
+    public static String getCurrentTenant(){
+        return CURRENT_TENANT.get();
+    }
+
+    public static void setCurrentTenant(String tenantId) {
+        CURRENT_TENANT.set(tenantId);
+    }
+
+    public static void clear() {
+        CURRENT_TENANT.remove();
+    }
+}
---
+++ src/main/java/com/coditas/powerbridge/tenant/TenantFilter.java
@@ -0,0 +1,28 @@
+package com.coditas.powerbridge.tenant;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class TenantFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String tenantId = request.getHeader("X-Tenant-ID");
+            if(!tenantId.isBlank())
+                TenantContext.setCurrentTenant(tenantId);
+            filterChain.doFilter(request, response);
+        }finally {
+            TenantContext.clear();
+        }
+    }
+}
